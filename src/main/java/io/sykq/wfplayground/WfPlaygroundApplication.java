1117b61583819e1a6ff8db7fa2ed805c12c27f0b
sykq <genericmailacc@gmail.com>
2020-02-27 17:53:57 +0100

    init

---
+++ src/main/java/io/sykq/wfplayground/WfPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package io.sykq.wfplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WfPlaygroundApplication.class, args);
+	}
+
+}
