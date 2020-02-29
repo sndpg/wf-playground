91a769546d07d0277d036af8b8ea1d8cf152046a
sykq <genericmailacc@gmail.com>
2020-02-29 21:08:25 +0100

    thymeleaf

--- src/main/java/io/sykq/wfplayground/controller/DataController.java
+++ src/main/java/io/sykq/wfplayground/controller/DataController.java
@@ -4,6 +4,7 @@ import io.sykq.wfplayground.model.MiscResponse;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +23,9 @@ public class DataController {
     private final FluxProcessor<MiscResponse, MiscResponse> miscResponsesFluxProcessor;
 
     private final Flux<ScoreCounter> sumFlux;
+    private final Flux<ScoreCounter> scoreStatus;
     private final ScoreCounter scoreCounter = new ScoreCounter();
+
 
     public DataController(FluxProcessor<MiscResponse, MiscResponse> miscResponsesFluxProcessor) {
         this.miscResponsesFluxProcessor = miscResponsesFluxProcessor;
@@ -38,6 +41,20 @@ public class DataController {
                 .doOnEach(s -> log.info("highScores: {}, lowScores: {}",
                         Objects.requireNonNull(s.get()).getHighScores(),
                         Objects.requireNonNull(s.get()).getLowScores()));
+
+        scoreStatus =
+                miscResponsesFluxProcessor
+                        .map(MiscResponse::getScore)
+                        .scan(new ScoreCounter(), (accumulator, score) -> {
+                            if (score.compareTo(50) < 0) {
+                                accumulator.incrementLowScoreCounter();
+                            } else {
+                                accumulator.incrementHighScoreCounter();
+                            }
+                            return accumulator;
+                        })
+                        .doOnNext(next -> log.info("reduced to -> high: {}, low: {}", next.getHighScores(),
+                                next.getLowScores()));
     }
 
     @GetMapping("/data")
@@ -47,11 +64,10 @@ public class DataController {
     }
 
     @GetMapping("/stream")
-    public String doSomething(Model model) {
-
-//        model.addAttribute("sum", scoreCounter.getHighScores() + scoreCounter.getLowScores());
+    public String streamedFlux(Model model) {
         model.addAttribute("scoreCounter",
                 new ReactiveDataDriverContextVariable(sumFlux, 1));
+
         //        model.addAttribute("lowScoreCount",
         //                new ReactiveDataDriverContextVariable(miscResponsesReplayProcessor, 1));
         //        model.addAttribute("data",
@@ -60,9 +76,22 @@ public class DataController {
         return "misc-log";
     }
 
+
+    @GetMapping(value = "/status")
+    public String status(Model model) {
+        model.addAttribute("scoreCounters", new ReactiveDataDriverContextVariable(scoreStatus, 1));
+        return "status";
+    }
+
+    @GetMapping(value = "/status-counter", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public String statusCounter(Model model) {
+        model.addAttribute("scoreCounters", new ReactiveDataDriverContextVariable(scoreStatus, 1));
+        return "status :: #counter";
+    }
+
     @EqualsAndHashCode
     @NoArgsConstructor
-    private static class ScoreCounter {
+    public static class ScoreCounter {
         private AtomicInteger lowScoreCounter = new AtomicInteger(0);
         private AtomicInteger highScoreCounter = new AtomicInteger(0);
 
@@ -74,11 +103,11 @@ public class DataController {
             highScoreCounter.incrementAndGet();
         }
 
-        public int getLowScores(){
+        public int getLowScores() {
             return lowScoreCounter.get();
         }
 
-        public int getHighScores(){
+        public int getHighScores() {
             return highScoreCounter.get();
         }
     }
--- src/main/java/io/sykq/wfplayground/controller/MiscRestController.java
+++ src/main/java/io/sykq/wfplayground/controller/MiscRestController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.FluxProcessor;
-import reactor.core.publisher.ReplayProcessor;
 
 import java.time.LocalDateTime;
 import java.util.Map;
